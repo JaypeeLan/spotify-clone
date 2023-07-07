257eb8167a692f9ff922bf7b6b7b297c89e546de
JaypeeLan <jplaniran01@gmail.com>
2023-07-07 11:32:40 +0100

    initial commit, layout

--- components/Box.tsx
+++ components/Box.tsx
@@ -5,7 +5,7 @@ import { twMerge } from "tailwind-merge";
 const Box: React.FC<BoxProps> = ({ children, className }) => {
   return (
     <div
-      className={twMerge(`bg-neutral-500 rounded-lg h-fit w-full`, className)}
+      className={twMerge(`bg-neutral-900 rounded-lg h-fit w-full`, className)}
     >
       {children}
     </div>
